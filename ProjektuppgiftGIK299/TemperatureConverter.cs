82e429c05f997ae306811128d0fc58f248cef8bf
Mikael <mikaelmarkus1000@gmail.com>
2024-01-03 14:16:03 +0100

    Added convertions and text

---
+++ ProjektuppgiftGIK299/TemperatureConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ProjektUppgiftGIK299
+{
+    // Klassen TemperatureConverter hanterar konvertering mellan olika temperatur-enheter.
+    public class TemperatureConverter
+    {
+        // Konverterar temperatur från Celsius till Fahrenheit.
+        public double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+
+        // Konverterar temperatur från Fahrenheit till Celsius.
+        public double FahrenheitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5 / 9;
+        }
+
+        // Konverterar temperatur från Celsius till Kelvin.
+        public double CelsiusToKelvin(double celsius)
+        {
+            return celsius + 273.15;
+        }
+
+        // Konverterar temperatur från Kelvin till Celsius.
+        public double KelvinToCelsius(double kelvin)
+        {
+            return kelvin - 273.15;
+        }
+    }
+}
